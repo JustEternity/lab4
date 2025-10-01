09b3561a18f43a9a4b01bb7103846ecfea6db3b4
Maxim Shirokov <maximshirokov2006@gmail.com>
2025-10-01 20:26:27 +0300

    add colorful UI

--- ui/addtask.py
+++ ui/addtask.py
@@ -8,12 +8,15 @@ class Ui_TaskWin(object):
         TaskWin.setMinimumSize(QtCore.QSize(405, 395))
         TaskWin.setMaximumSize(QtCore.QSize(405, 395))
         TaskWin.setStyleSheet("#TaskWin {\n"
-"    background-color: #f8f9fa;\n"
+"    background-color: #ccffff;\n"
 "    border: 1px solid #dee2e6;\n"
 "}")
         self.nametask = QtWidgets.QLineEdit(parent=TaskWin)
         self.nametask.setGeometry(QtCore.QRect(30, 50, 331, 21))
         self.nametask.setObjectName("nametask")
+        self.nametask.setStyleSheet("#nametask {\n"
+"    background-color: #FFFCE9;\n"
+"}")
         self.label = QtWidgets.QLabel(parent=TaskWin)
         self.label.setGeometry(QtCore.QRect(30, 20, 111, 16))
         font = QtGui.QFont()
@@ -26,15 +29,31 @@ class Ui_TaskWin(object):
         self.abouttask.setAccessibleDescription("")
         self.abouttask.setAutoFillBackground(False)
         self.abouttask.setObjectName("abouttask")
+        self.abouttask.setStyleSheet("#abouttask {\n"
+"    background-color: #FFFCE9;\n"
+"}")
         self.savetask = QtWidgets.QPushButton(parent=TaskWin)
         self.savetask.setGeometry(QtCore.QRect(260, 330, 101, 31))
         self.savetask.setObjectName("savetask")
+        self.savetask.setStyleSheet("#savetask {\n"
+"    background-color: #FFCC99;\n"
+"    border: 1px solid #dee2e6;\n"
+"    border-radius: 10%;\n"
+"}")
         self.deletetask = QtWidgets.QPushButton(parent=TaskWin)
         self.deletetask.setGeometry(QtCore.QRect(150, 330, 101, 31))
         self.deletetask.setObjectName("deletetask")
+        self.deletetask.setStyleSheet("#deletetask {\n"
+"    background-color: #FFCC99;\n"
+"    border: 1px solid #dee2e6;\n"
+"    border-radius: 10%;\n"
+"}")
         self.dateTimeEdit = QtWidgets.QDateTimeEdit(parent=TaskWin)
         self.dateTimeEdit.setGeometry(QtCore.QRect(30, 80, 194, 22))
         self.dateTimeEdit.setObjectName("dateTimeEdit")
+        self.dateTimeEdit.setStyleSheet("#dateTimeEdit {\n"
+"    background-color: #FFFCE9;\n"
+"}")
 
         self.retranslateUi(TaskWin)
         QtCore.QMetaObject.connectSlotsByName(TaskWin)
